43b23107c2252e01720fea2b77f86d2748fd60da
wB <66862193+a4rt@users.noreply.github.com>
2024-09-26 11:30:41 -0600

    Update Ejemplos.cs

--- Ejemplos.cs
+++ Ejemplos.cs
@@ -38,3 +38,54 @@ public class Program
 		
 	}
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//HErencia y polimorfismo
+using System;
+using System.Collections.Generic;
+using System.Collections;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var shapes = new List<Shape>();
+		var rectangle = new Rectangle(){Width=20, Height = 10};
+		var triangle = new Triangle(){Width=10, Height = 15};
+		var Square = new Square(){Width=10};
+		
+		shapes.Add(rectangle);
+		shapes.Add(triangle);
+		shapes.Add(Square);
+		
+		foreach(var item in shapes)
+			Console.WriteLine(item.CalculateArea());
+	}
+}
+
+public abstract class Shape{
+	public int Height {get; set;}
+	public int Width {get; set;}
+	public abstract double CalculateArea();
+}
+
+public class Square:Shape{
+	public override double CalculateArea(){
+		return Math.Pow(Width, 2);
+	}
+}
+
+public class Rectangle:Shape{
+	public override double CalculateArea(){
+		return Width*Height;
+	}
+}
+
+public class Triangle:Shape{
+	public override double CalculateArea(){
+		return (Width*Height)/2;
+	}
+}
+
+
